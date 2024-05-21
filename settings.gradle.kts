c8251e97da66c783e39d1104e2850ff3b7276a47
Matt Henk <mattoopiee@gmail.com>
2024-05-21 18:49:14 +0200

    Adds develocity instead of enterprise plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,15 +2,13 @@ rootProject.name = "fold"
 include("lib")
 
 plugins {
-    id("com.gradle.enterprise") version "3.17.4"
+    id("com.gradle.develocity") version "3.17.4"
 }
 
-gradleEnterprise {
-    if (System.getenv("CI") != null) {
-        buildScan {
-            publishAlways()
-            termsOfServiceUrl = "https://gradle.com/terms-of-service"
-            termsOfServiceAgree = "yes"
-        }
+develocity {
+    buildScan {
+        publishing.onlyIf { System.getenv("CI") != null }
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
     }
 }
